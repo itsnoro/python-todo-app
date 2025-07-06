1baad7f6e6c6cea028b50bdf3ee0a434ba16cd00
unknown <ferhaouinoro@gmail.com>
2025-07-06 12:31:07 +0100

    feat: implement add command for todo CLI

--- todo.py
+++ todo.py
@@ -21,14 +21,34 @@ def list_tasks():
         status = '✓' if t.get('done', False) else ' '
         print(f"[{t['id']}] [{status}] {t['desc']}")
 
+def add_task(description: str):
+    tasks = load_tasks()
+    # Determine next ID
+    next_id = max((t['id'] for t in tasks), default=0) + 1
+    new_task = {
+        'id': next_id,
+        'desc': description,
+        'done': False
+    }
+    tasks.append(new_task)
+    with open(TASKS_FILE, 'w') as f:
+        json.dump(tasks, f, indent=2)
+    print(f"Added task [{next_id}]: {description}")
+
 def main():
     parser = argparse.ArgumentParser(prog='todo')
     subparsers = parser.add_subparsers(dest='cmd')
     subparsers.add_parser('list', help='List all tasks')
+    # Add parser for "add" command
+    add_p = subparsers.add_parser('add', help='Add a new task')
+    add_p.add_argument('desc', metavar='DESCRIPTION', help='Task description')
+
     args = parser.parse_args()
 
     if args.cmd == 'list':
         list_tasks()
+    elif args.cmd == 'add':
+        add_task(args.desc)
     else:
         parser.print_help()
 
